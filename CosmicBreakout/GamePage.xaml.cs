fcf164d1dbd6651a034d210850fbb97477ef4c61
Marc King <mjking@umich.edu>
2017-04-16 21:04:39 -0400

    Added GamePage and button to take you to it from MainPage.

--- CosmicBreakout/GamePage.xaml.cs
+++ CosmicBreakout/GamePage.xaml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,25 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace CosmicBreakout
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class GamePage : Page
     {
-        public MainPage()
+        public GamePage()
         {
             this.InitializeComponent();
-            ApplicationView view = ApplicationView.GetForCurrentView();
-            view.TryEnterFullScreenMode();
-        }
-
-        private void StartGame_Click(object sender, RoutedEventArgs e)
-        {
-            Frame.Navigate(typeof(GamePage));
         }
     }
 }
